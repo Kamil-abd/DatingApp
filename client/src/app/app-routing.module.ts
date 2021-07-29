fe857164fecada9167e3f92b10945310b1ab8405
Kamil <kamilabdullaev1991@gmail.com>
2021-07-29 23:10:16 +0300

    User edit add

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,9 +7,11 @@ import { TestErrorsComponent } from 'src/app/errors/test-errors/test-errors.comp
 import { HomeComponent } from 'src/app/home/home.component';
 import { ListsComponent } from 'src/app/lists/lists.component';
 import { MemberDetailComponent } from 'src/app/members/member-detail/member-detail.component';
+import { MemberEditComponent } from 'src/app/members/member-edit/member-edit.component';
 import { MemberListComponent } from 'src/app/members/member-list/member-list.component';
 import { MessagesComponent } from 'src/app/messages/messages.component';
 import { AuthGuard } from 'src/app/_guards/auth.guard';
+import { PreventUnsavedChangesGuard } from 'src/app/_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,8 +20,9 @@ const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+      { path: 'members', component: MemberListComponent },
       { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'member/edit', component: MemberEditComponent , canDeactivate: [PreventUnsavedChangesGuard]},
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
